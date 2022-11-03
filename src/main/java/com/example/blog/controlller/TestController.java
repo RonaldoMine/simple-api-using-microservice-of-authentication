eb70b4df4bb5d1e0219d0aa4e07e8bba732a7f33
Ronaldo Mine <andremine98@gmail.com>
2022-11-03 18:13:26 +0100

    First Commit Of Project

---
+++ src/main/java/com/example/blog/controlller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.blog.controlller;
+
+import com.example.blog.security.service.UserAuth;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        UserAuth user  = (UserAuth) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return "Hello World "+user.getUsername()+", your id is : "+user.getId()+", your role is "+user.getRoles();
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String admin() {
+        UserAuth user  = (UserAuth) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return "Hello World Admin "+user.getUsername()+", your id is : "+user.getId()+", your role is "+user.getRoles();
+    }
+}
