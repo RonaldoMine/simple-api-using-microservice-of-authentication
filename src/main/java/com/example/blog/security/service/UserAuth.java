be5a281cf6e63c280dda31f6086ff76b405ff342
Ronaldo Mine <andremine98@gmail.com>
2022-11-14 22:16:52 +0100

    _

--- src/main/java/com/example/blog/security/service/UserAuth.java
+++ src/main/java/com/example/blog/security/service/UserAuth.java
@@ -13,6 +13,10 @@ import java.util.List;
 public class UserAuth implements UserDetailsService {
     Long id;
     String username;
+    String email;
+    String first_name;
+    String last_name;
+    String phone;
     List<String> roles;
 
     @Override
